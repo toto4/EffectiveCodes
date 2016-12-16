7a6f5b401f21309e8df393a183394606fa289d4a
wangyongzhen sam <15950040347@162.com>
2016-12-16 14:53:14 +0800

    init

---
+++ EffectiveCodesDemo/EffectiveCodesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EffectiveCodesDemo
+//
+//  Created by wangyongzhen sam on 2016/12/16.
+//  Copyright © 2016年 samPersonal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
