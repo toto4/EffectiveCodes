fec457ea1dc441c513c8366c4685e86c243e4f9b
wangyongzhen sam <15950040347@162.com>
2016-12-20 13:09:55 +0800

    add libs

---
+++ EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.h
@@ -0,0 +1,13 @@
+//
+//  SAMDBHelper.h
+//  EffectiveCodesDemo
+//
+//  Created by wangyongzhen sam on 2016/12/18.
+//  Copyright © 2016年 samPersonal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAMDBHelper : NSObject
+
+@end
---
+++ EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.m
@@ -0,0 +1,13 @@
+//
+//  SAMDBHelper.m
+//  EffectiveCodesDemo
+//
+//  Created by wangyongzhen sam on 2016/12/18.
+//  Copyright © 2016年 samPersonal. All rights reserved.
+//
+
+#import "SAMDBHelper.h"
+
+@implementation SAMDBHelper
+
+@end
