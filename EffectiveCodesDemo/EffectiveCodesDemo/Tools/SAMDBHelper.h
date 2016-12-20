418c98ef769db00fc2d3f05d21a68fc44e0d8280
wangyongzhen sam <15950040347@162.com>
2016-12-20 16:00:48 +0800

    dbhelper

--- EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.h
+++ EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.h
@@ -6,8 +6,46 @@
 //  Copyright © 2016年 samPersonal. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface SAMDBHelper : NSObject
+@class FMResultSet,FMDatabaseQueue;
+
+@interface UIApplication (SAMDBExtension)
+
+@property(nonatomic , strong) FMDatabaseQueue * userDBQueue;
+
+@property(nonatomic , strong) FMDatabaseQueue * publicDBQueue;
+
+
+
+#pragma mark - setup DB Queue
+
+// Call this method in 'application:didFinishLaunchingWithOptions:'
+- (void)sam_setupUserDBQueueWithDBPath:(NSString *)DBPathString;
+
+// Call this method in 'application:didFinishLaunchingWithOptions:'
+- (void)sam_setupPublicDBQueueWithDBPath:(NSString *)DBPathString;
+
+
 
 @end
+
+
+
+
+BOOL isDBStringEmpty(NSString *DBString);
+
+
+
+/** nil 转 @"" */
+NSString * dbStringConverter(NSString *string);
+/** @"" 转 nil */
+NSString * dbStringReverse(NSString *string);
+
+// YES 转 1；NO 转 0
+NSInteger dbBooleanConverter(BOOL boolean);
+
+// 1 转 YES；0 转 NO
+BOOL dbBooleanReverse(NSInteger i);
+
+
--- EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.m
+++ EffectiveCodesDemo/EffectiveCodesDemo/Tools/SAMDBHelper.m
@@ -8,6 +8,100 @@
 
 #import "SAMDBHelper.h"
 
-@implementation SAMDBHelper
+#import "FMDB.h"
+#import <objc/runtime.h>
+
+FMDatabaseQueue * privateDBQueue();
+FMDatabaseQueue * publicDBQueue() ;
+
+
+static const void *kUserDBQueueKey = &kUserDBQueueKey;
+
+static const void *kPublicDBQueueKey = &kPublicDBQueueKey;
+
+@implementation UIApplication (SAMDBExtension)
+
+- (void)sam_setupUserDBQueueWithDBPath:(NSString *)DBPathString
+{
+    if (nil == DBPathString || 0 == DBPathString.length) {
+        return;
+    }
+    
+    FMDatabaseQueue *queue = [[FMDatabaseQueue alloc] initWithPath:DBPathString];
+    self.userDBQueue =  queue;
+}
+
+
+- (void)sam_setupPublicDBQueueWithDBPath:(NSString *)DBPathString
+{
+    if (nil == DBPathString || 0 == DBPathString.length) {
+        return;
+    }
+    
+    FMDatabaseQueue *queue = [[FMDatabaseQueue alloc] initWithPath:DBPathString];
+    self.publicDBQueue =  queue;
+}
+
+
+
+#pragma mark - setter & getter
+
+- (void)setUserDBQueue:(FMDatabaseQueue *)userDBQueue
+{
+    objc_setAssociatedObject(self, kUserDBQueueKey, userDBQueue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (FMDatabaseQueue *)userDBQueue
+{
+    return objc_getAssociatedObject(self, kUserDBQueueKey);
+}
+
+
+- (void)setPublicDBQueue:(FMDatabaseQueue *)publicDBQueue
+{
+    objc_setAssociatedObject(self, kPublicDBQueueKey, publicDBQueue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (FMDatabaseQueue *)publicDBQueue
+{
+    return objc_getAssociatedObject(self, kPublicDBQueueKey);
+}
+
 
 @end
+
+
+BOOL isDBStringEmpty(NSString *DBString) {
+    
+    return [DBString isEqualToString:@""] ? YES : NO;
+}
+
+NSString * dbStringConverter(NSString *string) {
+    
+    return string == nil ? @"" : string;
+}
+
+NSString * dbStringReverse(NSString *string) {
+    
+    return [string isEqualToString:@""] ? nil : string;
+}
+
+NSInteger dbBooleanConverter(BOOL boolean) {
+    
+    return boolean ? 1 : 0;
+}
+
+BOOL dbBooleanReverse(NSInteger i) {
+    
+    return i == 0 ? NO : YES;
+}
+
+FMDatabaseQueue * privateDBQueue()
+{
+    return [UIApplication sharedApplication].userDBQueue;
+}
+FMDatabaseQueue * publicDBQueue()
+{
+    return [UIApplication sharedApplication].publicDBQueue;
+}
+
+
+
